146e187578dc95a36381ee0d5e9151a73403cded
Szucs Attila <szucsa@sonrisa.hu>
2019-05-14 14:56:10 +0200

    Add project files.

---
+++ Brainfuck.NET/Precompiler.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainfuck.NET
+{
+    public class Precompiler
+    {
+        public struct OPS
+        {
+            public States s;
+            public int v;
+        }
+
+        private List<OPS> ops = new List<OPS>();
+
+        public enum States
+        {
+            DATA,
+            PTR,
+            OPEN_LOOP,
+            CLOSE_LOOP,
+            PRINT,
+            READ,
+            NONE,
+        }
+
+        private void StateChanged(States s, int value)
+        {
+            var o = new OPS
+            {
+                s = s,
+                v = value
+            };
+
+            ops.Add(o);
+        }
+
+        public Precompiler(string path)
+        {
+
+            var bfSource = File.ReadAllText(path);
+
+            var value = 0;
+            var state = States.NONE;
+
+            foreach (var symbol in bfSource)
+            {
+                switch (symbol)
+                {
+                    case '+':
+                        if (state == States.DATA)
+                        {
+                            value++;
+                        }
+                        else
+                        {
+                            StateChanged(state,value);
+                            value = 1;
+                            state = States.DATA;
+                        }
+                        break;
+                    case '-':
+                        if (state == States.DATA)
+                        {
+                            value--;
+                        }
+                        else
+                        {
+                            StateChanged(state, value);
+                            value = -1;
+                            state = States.DATA;
+                        }
+                        break;
+                    case '<':
+                        if (state == States.PTR)
+                        {
+                            value--;
+                        }
+                        else
+                        {
+                            StateChanged(state, value);
+                            value = -1;
+                            state = States.PTR;
+                        }
+                        break;
+                    case '>':
+                        if (state == States.PTR)
+                        {
+                            value++;
+                        }
+                        else
+                        {
+                            StateChanged(state, value);
+                            value = 1;
+                            state = States.PTR;
+                        }
+                        break;
+                    case '[':
+                        StateChanged(state, value);
+                        state = States.OPEN_LOOP;
+                        value = 0;
+                        break;
+                    case ']':
+                        StateChanged(state, value);
+                        state = States.CLOSE_LOOP;
+                        value = 0;
+                        break;
+                    case '.':
+                        StateChanged(state, value);
+                        state = States.PRINT;
+                        value = 0;
+                        break;
+                    case ',':
+                        StateChanged(state, value);
+                        state = States.READ;
+                        value = 0;
+                        break;
+                }
+            }
+            StateChanged(state, value);
+        }
+
+        public IEnumerable<OPS> Precompiled
+        {
+            get
+            {
+                return this.ops;
+            }
+        }
+
+    }
+}
---
+++ Brainfuck.NET/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.IO;
+
+namespace Brainfuck.NET
+{
+    class Program
+    {
+        class Labels
+        {
+            public Label startOfBlock;
+            public Label endOfBlock;
+        }
+
+        static void Main(string[] args)
+        {
+            var labels = new Stack<Labels>();
+
+            var an = new AssemblyName("Brainfuck.NET");
+            var dom = AppDomain.CurrentDomain;
+            var builder = dom.DefineDynamicAssembly(an, AssemblyBuilderAccess.Save);
+            var mb = builder.DefineDynamicModule(an.Name, "BF.exe", false);
+            var tb = mb.DefineType("Brainfuck.Application", TypeAttributes.Public | TypeAttributes.Class);
+            var methodBuilder = tb.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, typeof(int), new Type[] { });
+            var gen = methodBuilder.GetILGenerator();
+
+            var dataMethod = tb.DefineMethod("AddToCell", MethodAttributes.Private | MethodAttributes.Static, typeof(void), new Type[] { });
+
+            /*gen.Emit(OpCodes.Ldstr, "Hello world");
+            gen.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new Type[] { typeof(string) }));
+            gen.Emit(OpCodes.Call, typeof(Console).GetMethod("ReadKey", new Type[] { }));
+            gen.Emit(OpCodes.Pop);
+            gen.Emit(OpCodes.Ldc_I4_0);
+            gen.Emit(OpCodes.Ret);*/
+
+            var p = new Precompiler("mandelbrot.bf");
+            var precompiled = p.Precompiled;
+
+            var CellBuilder = gen.DeclareLocal(typeof(byte[]));
+            //CellBuilder.SetLocalSymInfo("Cells");
+            gen.Emit(OpCodes.Ldc_I4, 30000);
+            gen.Emit(OpCodes.Newarr, typeof(byte));
+            gen.Emit(OpCodes.Stloc_S, CellBuilder);
+
+            var DataPointerBuilder = gen.DeclareLocal(typeof(int));
+            //DataPointerBuilder.SetLocalSymInfo("DataPointer");
+            gen.Emit(OpCodes.Ldc_I4_0);
+            gen.Emit(OpCodes.Stloc, DataPointerBuilder);
+
+            foreach (var symbol in precompiled)
+            {
+                switch (symbol.s)
+                {
+                    case Precompiler.States.DATA:
+                        gen.Emit(OpCodes.Ldloc, CellBuilder);
+                        gen.Emit(OpCodes.Ldloc, DataPointerBuilder);
+                        gen.Emit(OpCodes.Ldloc, CellBuilder);
+                        gen.Emit(OpCodes.Ldloc, DataPointerBuilder);
+                        gen.Emit(OpCodes.Ldelem_I1);
+                        gen.Emit(OpCodes.Ldc_I4, symbol.v);
+                        gen.Emit(OpCodes.Add);
+                        gen.Emit(OpCodes.Stelem_I1);
+                        break;
+                    case Precompiler.States.PTR:
+                        gen.Emit(OpCodes.Ldloc, DataPointerBuilder);
+                        gen.Emit(OpCodes.Ldc_I4, symbol.v);
+                        gen.Emit(OpCodes.Add);
+                        gen.Emit(OpCodes.Stloc, DataPointerBuilder);
+                        break;
+                    case Precompiler.States.OPEN_LOOP:
+                        {
+                            var blockBorders = new Labels
+                            {
+                                startOfBlock = gen.DefineLabel(),
+                                endOfBlock = gen.DefineLabel()
+                            };
+
+                            gen.Emit(OpCodes.Ldloc, CellBuilder);
+                            gen.Emit(OpCodes.Ldloc, DataPointerBuilder);
+                            gen.Emit(OpCodes.Ldelem_I1);
+                            gen.Emit(OpCodes.Brfalse, blockBorders.endOfBlock);
+                            gen.MarkLabel(blockBorders.startOfBlock);
+                            labels.Push(blockBorders);
+                            break;
+                        }
+                    case Precompiler.States.CLOSE_LOOP:
+                        {
+                            var blockBorders = labels.Pop();
+                            gen.Emit(OpCodes.Ldloc, CellBuilder);
+                            gen.Emit(OpCodes.Ldloc, DataPointerBuilder);
+                            gen.Emit(OpCodes.Ldelem_I1);
+                            gen.Emit(OpCodes.Brtrue, blockBorders.startOfBlock);
+                            gen.MarkLabel(blockBorders.endOfBlock);
+                            break;
+                        }
+                    case Precompiler.States.PRINT:
+                        gen.Emit(OpCodes.Ldloc, CellBuilder);
+                        gen.Emit(OpCodes.Ldloc, DataPointerBuilder);
+                        gen.Emit(OpCodes.Ldelem_I1);
+                        gen.Emit(OpCodes.Call, typeof(Console).GetMethod("Write", new Type[] { typeof(char) }));
+                        break;
+                    case Precompiler.States.READ:
+                        gen.Emit(OpCodes.Ldloc, CellBuilder);
+                        gen.Emit(OpCodes.Ldloc, DataPointerBuilder);
+                        gen.Emit(OpCodes.Call, typeof(Console).GetMethod("ReadKey", new Type[] { }));
+                        gen.Emit(OpCodes.Stelem_I1);
+                        break;
+                    default:
+                        //Skip symbol
+                        break;
+                }
+            }
+            gen.Emit(OpCodes.Call, typeof(Console).GetMethod("ReadKey", new Type[] { }));
+            gen.Emit(OpCodes.Pop);
+
+            gen.Emit(OpCodes.Ldc_I4_0);
+            gen.Emit(OpCodes.Ret);
+
+
+            var t = tb.CreateType();
+            builder.SetEntryPoint(methodBuilder, PEFileKinds.ConsoleApplication);
+            builder.Save("BF.exe");
+        }
+    }
+}
